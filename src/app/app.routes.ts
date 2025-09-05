cda7daed5541836884c65a73520fe158f48c559a
bene-byte <benedict.enjambre@synacy.com>
2025-09-05 13:06:01 +0800

    Feat: Edit Users Component no code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
-import {CreateUserComponent} from './hr/create-user/create-user.component';
-import {AppComponent} from './app.component';
+import { CreateUserComponent } from './hr/create-user/create-user.component';
+import {EditUserComponent} from './hr/edit-user/edit-user.component';
 
 export const routes: Routes = [
   {
-    path:'hr',
-    children:[
-      {
-        path:'create-employee', component: CreateUserComponent
-      }
+    path: 'hr',
+    children: [
+      { path: 'create-employee', component: CreateUserComponent  },
+      { path: 'employee/:id/edit', component: EditUserComponent }
     ]
-  }
+  },
+  { path: '**', redirectTo: 'hr/employees' }
 ];
