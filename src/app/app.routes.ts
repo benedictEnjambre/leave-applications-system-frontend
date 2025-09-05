0acd4df3d61d26c38779b01dd6e8ded1b5444eea
bene-byte <benedict.enjambre@synacy.com>
2025-09-05 16:48:42 +0800

    Nothing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CreateUserComponent } from './hr/create-user/create-user.component';
 import {EditUserComponent} from './hr/edit-user/edit-user.component';
+import {UserComponent} from './user/user.component';
 
 export const routes: Routes = [
   {
@@ -8,6 +9,7 @@ export const routes: Routes = [
     children: [
       { path: 'create-employee', component: CreateUserComponent  },
       { path: 'employee/:id/edit', component: EditUserComponent }
+
     ]
   },
   { path: '**', redirectTo: 'hr/employees' }
