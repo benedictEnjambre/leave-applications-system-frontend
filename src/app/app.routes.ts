c3ae8611e0cb705d81f6df82e9eba51974b60fde
mariedasian22 <marie.dasian@synacy.com>
2025-09-06 03:24:44 +0800

    add leave application form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CreateUserComponent } from './hr/create-user/create-user.component';
 import {EditUserComponent} from './hr/edit-user/edit-user.component';
 import {UserComponent} from './user/user.component';
 import {UsersListComponent} from './hr/user-list/user-list.component';
+import {AddLeaveComponent} from './manager/add-leave/add-leave.component';
 
 export const routes: Routes = [
   {
@@ -11,8 +12,16 @@ export const routes: Routes = [
       {path:'employees',component:UsersListComponent},
       { path: 'create-employee', component: CreateUserComponent  },
       { path: 'employee/:id/edit', component: EditUserComponent }
-
     ]
   },
-  { path: '**', redirectTo: 'hr/employees' }
+
+  {
+    path: 'manager',
+    children: [
+      {path: 'add-leave', component: AddLeaveComponent},
+
+    ]
+
+  },
+  { path: '**', redirectTo: 'hr/employees' },
 ];
